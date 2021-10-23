865c394f05f5cec8255099df646173a6c7faf948
BunnyMeee <75510733+BunnyMeee@users.noreply.github.com>
2021-10-22 18:44:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,43 @@ recognition.onresult = function run(event) {
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie") {
+        console.log("taking selfie");
+        speak();
+    }
 }
+function speak() {
+    var synth = window.speechSynthesis;
+    speakData = "taking your selfie in 5 seconds";
+    var utterthis = new SpeechSynthesisUtterance(speakData);
+    synth.speak(utterthis);
+    Webcam.attach(camera);
+    setTimeout(function () {
+        takeSnapshot();
+        save();
+
+    }, 5000);
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+
+});
+
+camera = document.getElementById("camera");
+
+function takeSnapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '">';
+    })
+}
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+
+}
+
